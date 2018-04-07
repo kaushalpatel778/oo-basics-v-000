d88072edc98e12d814c614d172cedfafd75b2756
Kaushal Patel <kaushalpatel778@gmail.com>
2018-04-07 02:51:55 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -35,3 +35,24 @@ end
 
 book = Book.new("Some Title")
 book.turn_page
+
+=begin
+# book.rb
+# USING ATTRIBUTE ACCESSORS AND ATTRIBUTE READERS
+class Book
+  attr_accessor :author, :page_count, :genre
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+=end
