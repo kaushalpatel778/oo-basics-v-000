da4618e05f4d6f63ae4572c10f68a741af8a919c
Kaushal Patel <kaushalpatel778@gmail.com>
2018-04-07 02:47:40 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -17,8 +17,15 @@ class Book
       def page_count=(num)
         @page_count = num
       end
-
+      
       def page_count
         @page_count
+      end
+      def genre=(genre)
+          @genre = genre
+        end
+       
+      def genre
+        @genre
       end
 end
