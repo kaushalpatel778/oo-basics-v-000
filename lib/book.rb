6a6d3b88805685c9e07331905e644cfb0a9f0812
Kaushal Patel <kaushalpatel778@gmail.com>
2018-04-07 02:48:03 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,7 +3,6 @@ class Book
       def initialize(title)
         @title = title
       end
-
       def title
         @title
       end
@@ -14,17 +13,17 @@ class Book
       def author= (author)
         @author = author
       end
+
       def page_count=(num)
         @page_count = num
       end
-      
       def page_count
         @page_count
       end
+
       def genre=(genre)
           @genre = genre
         end
-       
       def genre
         @genre
       end
